8deda2b4e26fb9ea2afff785f7e1c61b4c75d443
davidgao325 <50885792+davidgao325@users.noreply.github.com>
2021-08-21 13:06:36 -0400

    Update nba_model_building.py

--- nba_model_building.py
+++ nba_model_building.py
@@ -35,24 +35,19 @@ regressor = LinearRegression(fit_intercept =True, normalize=True)
 regressor.fit(x_train, y_train)
 pred = regressor.predict(x_test)
 r2_score1 = r2_score(y_test, pred)
-mean_square_error_1 = mean_squared_error(y_test, pred)
-mean_absolute_error1 = mean_absolute_error(y_test, pred)
 
 x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size = 0.2, random_state = 42)
 lr = RandomForestRegressor(n_estimators=50)
 lr.fit(x_train, y_train)
 prediction = lr.predict(x_test)
 r2_score2 = r2_score(y_test, prediction)
-mean_square_error_2 = mean_squared_error(y_test, prediction)
-mean_absolute_error2 = mean_absolute_error(y_test, prediction)
 
 x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size = 0.2, random_state = 42)
 reg = AdaBoostRegressor(DecisionTreeRegressor(max_depth =5), n_estimators =500)
 adaboost = reg.fit(x_train, y_train)
 p = adaboost.predict(x_test)
 r2_score3 = r2_score(y_test, p)
-mean_square_error_3 = mean_squared_error(y_test, p)
-mean_absolute_error3 = mean_absolute_error(y_test, p)
+
 
 
 
